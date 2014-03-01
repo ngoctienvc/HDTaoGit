2f03ee04401eb7ff1437b3cdeb3fb8e38eeb36d0
Ngoc Tien <ngoctienvc@gmail.com>
2014-03-01 11:49:34 +0700

    UpLoad

---
+++ Baitap2/Baitap2/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Baitap2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxGiaTri = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxMang = new System.Windows.Forms.ComboBox();
+            this.buttonThem = new System.Windows.Forms.Button();
+            this.buttonXoa = new System.Windows.Forms.Button();
+            this.buttonSua = new System.Windows.Forms.Button();
+            this.buttonGhifile = new System.Windows.Forms.Button();
+            this.buttonDocfile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gia tri";
+            // 
+            // textBoxGiaTri
+            // 
+            this.textBoxGiaTri.Location = new System.Drawing.Point(87, 22);
+            this.textBoxGiaTri.Name = "textBoxGiaTri";
+            this.textBoxGiaTri.Size = new System.Drawing.Size(89, 20);
+            this.textBoxGiaTri.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(219, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mang";
+            // 
+            // comboBoxMang
+            // 
+            this.comboBoxMang.FormattingEnabled = true;
+            this.comboBoxMang.Location = new System.Drawing.Point(288, 22);
+            this.comboBoxMang.Name = "comboBoxMang";
+            this.comboBoxMang.Size = new System.Drawing.Size(131, 21);
+            this.comboBoxMang.TabIndex = 3;
+            // 
+            // buttonThem
+            // 
+            this.buttonThem.Location = new System.Drawing.Point(39, 75);
+            this.buttonThem.Name = "buttonThem";
+            this.buttonThem.Size = new System.Drawing.Size(62, 24);
+            this.buttonThem.TabIndex = 4;
+            this.buttonThem.Text = "Them";
+            this.buttonThem.UseVisualStyleBackColor = true;
+            this.buttonThem.Click += new System.EventHandler(this.buttonThem_Click);
+            // 
+            // buttonXoa
+            // 
+            this.buttonXoa.Location = new System.Drawing.Point(107, 75);
+            this.buttonXoa.Name = "buttonXoa";
+            this.buttonXoa.Size = new System.Drawing.Size(69, 25);
+            this.buttonXoa.TabIndex = 5;
+            this.buttonXoa.Text = "Xoa";
+            this.buttonXoa.UseVisualStyleBackColor = true;
+            this.buttonXoa.Click += new System.EventHandler(this.buttonXoa_Click);
+            // 
+            // buttonSua
+            // 
+            this.buttonSua.Location = new System.Drawing.Point(183, 75);
+            this.buttonSua.Name = "buttonSua";
+            this.buttonSua.Size = new System.Drawing.Size(68, 25);
+            this.buttonSua.TabIndex = 6;
+            this.buttonSua.Text = "Sua";
+            this.buttonSua.UseVisualStyleBackColor = true;
+            this.buttonSua.Click += new System.EventHandler(this.buttonSua_Click);
+            // 
+            // buttonGhifile
+            // 
+            this.buttonGhifile.Location = new System.Drawing.Point(257, 74);
+            this.buttonGhifile.Name = "buttonGhifile";
+            this.buttonGhifile.Size = new System.Drawing.Size(69, 26);
+            this.buttonGhifile.TabIndex = 7;
+            this.buttonGhifile.Text = "Ghifile";
+            this.buttonGhifile.UseVisualStyleBackColor = true;
+            this.buttonGhifile.Click += new System.EventHandler(this.buttonGhifile_Click);
+            // 
+            // buttonDocfile
+            // 
+            this.buttonDocfile.Location = new System.Drawing.Point(332, 74);
+            this.buttonDocfile.Name = "buttonDocfile";
+            this.buttonDocfile.Size = new System.Drawing.Size(74, 25);
+            this.buttonDocfile.TabIndex = 8;
+            this.buttonDocfile.Text = "Doc file";
+            this.buttonDocfile.UseVisualStyleBackColor = true;
+            this.buttonDocfile.Click += new System.EventHandler(this.buttonDocfile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 134);
+            this.Controls.Add(this.buttonDocfile);
+            this.Controls.Add(this.buttonGhifile);
+            this.Controls.Add(this.buttonSua);
+            this.Controls.Add(this.buttonXoa);
+            this.Controls.Add(this.buttonThem);
+            this.Controls.Add(this.comboBoxMang);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxGiaTri);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Mang phan so";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxGiaTri;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxMang;
+        private System.Windows.Forms.Button buttonThem;
+        private System.Windows.Forms.Button buttonXoa;
+        private System.Windows.Forms.Button buttonSua;
+        private System.Windows.Forms.Button buttonGhifile;
+        private System.Windows.Forms.Button buttonDocfile;
+    }
+}
+
---
+++ Baitap2/Baitap2/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Baitap2
+{
+    public partial class Form1 : Form
+    {
+        class PhanSo
+        {
+            private int _tuso;
+            public int TuSo
+            {
+                get { return _tuso; }
+                set { _tuso = value; }
+            }
+            private int _mauso;
+            public int MauSo
+            {
+                get { return _mauso; }
+                set { _mauso = value; }
+            }
+            public PhanSo()
+            {
+                _tuso = 0;
+                _mauso = 1;
+            }
+            public PhanSo(string chuoi)
+            {
+                string[] mang = chuoi.Split('/');
+                _tuso = int.Parse(mang[0]);
+                _mauso = int.Parse(mang[1]);
+            }
+            public static bool KiemTraChuoiPhanSo(string chuoi)
+            {
+                string[] mang = chuoi.Split('/');
+                if (mang.Length != 2) return false;
+                int number;
+                if (int.TryParse(mang[0], out number) == false) return false;
+                if (int.TryParse(mang[1], out number) == false) return false;
+                return true;
+            }
+            public override string ToString()
+            {
+                string chuoi = TuSo.ToString() + "/" + MauSo.ToString();
+                return chuoi;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonThem_Click(object sender, EventArgs e)
+        {
+            string chuoi = textBoxGiaTri.Text;
+            PhanSo phanso1;
+            if (PhanSo.KiemTraChuoiPhanSo(chuoi) == true)
+            {
+                phanso1 = new PhanSo(chuoi);
+                comboBoxMang.Items.Add(phanso1);
+            }
+            else
+            {
+                MessageBox.Show("Sai du lieu nhap", "Thong bao");
+            }
+        }
+
+        private void buttonXoa_Click(object sender, EventArgs e)
+        {
+            if (comboBoxMang.SelectedIndex != -1)
+            {
+                comboBoxMang.Items.RemoveAt(comboBoxMang.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Ban chua chon phan tu can xoa", "Thong bao");
+            }
+        }
+
+        private void buttonSua_Click(object sender, EventArgs e)
+        {
+            if (comboBoxMang.SelectedIndex != -1)
+            {
+                string str1 = textBoxGiaTri.Text;
+                PhanSo ps = new PhanSo(str1);
+                comboBoxMang.Items[comboBoxMang.SelectedIndex] = ps;
+            }
+            else
+            {
+                MessageBox.Show("Ban chua chon phan tu can sua", "Thong bao");
+            }
+        }
+
+        private void buttonGhifile_Click(object sender, EventArgs e)
+        {
+            // hien thi dialog ghi file
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Ghi tap tin txt|*.txt";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                // lay duong dan file can ghi
+                string duongdan = dialog.FileName;
+                // ghi mang xuong file
+                StreamWriter ghi = new StreamWriter(duongdan);
+                for (int i = 0; i < comboBoxMang.Items.Count; i++)
+                {
+
+                }
+                for (int i = 0; i < comboBoxMang.Items.Count; i++)
+                {
+                    ghi.WriteLine(comboBoxMang.Items[i]);
+                }
+                ghi.Close();
+            }
+        }
+
+        private void buttonDocfile_Click(object sender, EventArgs e)
+        {
+            // xoa het cac phan tu dang co combobox
+            comboBoxMang.Items.Clear();
+            // hien thi dialog doc file
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Doc tap tin txt|*.txt";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                // lay duong dan file can doc
+                string duongdan = dialog.FileName;
+                // doc mang tu file
+                StreamReader doc = new StreamReader(duongdan);
+                PhanSo phanso1;
+                while (!doc.EndOfStream)
+                {
+                    string str = doc.ReadLine();
+                    phanso1 = new PhanSo(str);
+                    comboBoxMang.Items.Add(str);
+                }
+
+                doc.Close();
+            }
+        }
+
+       
+    }
+}
